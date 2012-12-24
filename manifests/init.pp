45f3be1e51ea1b32a61a8e92ea4f1eb04f8e95b9
David Winter <i@djw.me>
2012-12-24 13:18:08 +0000

    Add version.txt and propeties file.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,48 @@
 class minecraft (
-		$install_path = '/home/ubuntu',
+		$minecraft_version,
+		$install_path = '/home/ubuntu/minecraft',
 		$jar_name = 'minecraft_server.jar',
 		$jar_source = 'https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar',
 		$max_ram = 1024,
 		$startup_template = 'minecraft/minecraft-server.service.conf.erb',
+		$properties_template = 'minecraft/minecraft-server.properties.erb',
 	) {
 	
+	$owner_user = 'ubuntu'
 	$full_path = "${install_path}/${jar_name}"
 
 	package { 'openjdk-7-jre':
 		ensure => present,
 	}
 
+	file { 'directory':
+		path    => $install_path,
+		ensure  => directory,
+		owner   => $owner_user,
+	}
+
 	exec { 'download minecraft_server':
 		command => "wget -O ${full_path} ${jar_source}",
-		unless => "test -f ${full_path}",
+		unless  => "test `cat ${install_path}/version.txt` = '${minecraft_version}'",
+		require => File['directory'],
+	}
+
+	file { 'version':
+		require => [
+			File['directory'],
+			Exec['download minecraft_server'],
+		],
+		path => "${install_path}/version.txt",
+		content => $minecraft_version,
+		notify => Service['minecraft'],
+	}
+
+	file { 'server.properties':
+		ensure => file,
+		content => template($properties_template),
+		path => "${install_path}/server.properties",
+		require => File['directory'],
+		notify => Service['minecraft'],
 	}
 
 	file { 'startup script':
@@ -22,7 +50,8 @@ class minecraft (
 		content => template($startup_template),
 		require => [
 			Package['openjdk-7-jre'],
-			Exec['download minecraft_server'],
+			File['version'],
+			File['server.properties'],
 		],
 	}
 
